e7b6df4696e72ab522e7c141678226d3c734140d
tlatlsgksp <130515272+tlatlsgksp@users.noreply.github.com>
2023-10-14 23:02:56 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
   require('./route701');
